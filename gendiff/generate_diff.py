e57baaa35c10642062e005e3f33e159e1f6dddb9
Elena Shlius <eur1ezs@gmail.com>
2022-09-25 13:11:12 +0300

    1st - 3rd step: comparing flat data

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,35 @@
+import json
+
+
+def get_dictionary(file_path):
+    with open(file_path, 'r') as file:
+        dictionary = json.load(file)
+        return dictionary
+
+
+def convert_boolean(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    return value
+
+
+def generate_diff(file_path1, file_path2):
+    data1 = get_dictionary(file_path1)
+    data2 = get_dictionary(file_path2)
+    key_set = data1.keys() | data2.keys()
+    keys = sorted(list(key_set))
+    result = '{'
+    for key in keys:
+        if key not in data1:
+            result = f'{result}\n+ {key}: {convert_boolean(data2.get(key))}'
+        elif key not in data2:
+            result = f'{result}\n- {key}: {convert_boolean(data1.get(key))}'
+        elif data1.get(key) == data2.get(key):
+            result = f'{result}\n  {key}: {convert_boolean(data1.get(key))}'
+        else:
+            result = f'{result}\n' \
+                     f'- {key}: {convert_boolean(data1.get(key))}\n' \
+                     f'+ {key}: {convert_boolean(data2.get(key))}'
+    return f"{result}\n}}"
+
+#print(generate_diff('/home/elena/python-project-50/Files/file1.json', '/home/elena/python-project-50/Files/file2.json'))
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+import argparse
+
+from .. generate_diff import generate_diff
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str, help='Inputs path to the first compared file')
+    parser.add_argument('second_file', type=str, help='Inputs path to the second compared file')
+    parser.add_argument('-f', '--format', type=str, help='Set format of output')
+    args = parser.parse_args()
+    file_path1 = args.first_file
+    file_path2 = args.second_file
+    print(generate_diff(file_path1, file_path2))
+
+
+if __name__ == '__main__':
+    main()
